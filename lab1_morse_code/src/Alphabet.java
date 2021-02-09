98b864932e122caaaa98ffa2f7e26562cdc7860e
dyuganov-n <dyuganov.n@gmail.com>
2021-02-09 17:18:58 +0700

    minor changes

--- lab1_morse_code/src/Alphabet.java
+++ lab1_morse_code/src/Alphabet.java
@@ -11,21 +11,20 @@ public class Alphabet {
     public Alphabet(String fileName) throws java.io.IOException {
         final int expectedSpacePos = 1;
 
-        BufferedReader in = new BufferedReader(new FileReader(fileName));
-        charToMorse = new HashMap<Character, String>();
-        morseToChar = new HashMap<String, Character>();
+        try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
+            charToMorse = new HashMap<Character, String>();
+            morseToChar = new HashMap<String, Character>();
 
-        String str;
-        while((str = in.readLine()) != null){
-            if(str.indexOf(' ') != expectedSpacePos || str.isEmpty()){
-                throw new IOException("Alphabet file has wrong format");
+            String str;
+            while ((str = in.readLine()) != null) {
+                if (str.indexOf(' ') != expectedSpacePos || str.isEmpty()) {
+                    throw new IOException("Alphabet file has wrong format");
+                }
+                String[] splitStr = str.split(" ");
+                charToMorse.put(str.charAt(0), splitStr[1]);
+                morseToChar.put(splitStr[1], str.charAt(0));
             }
-            String[] splitStr = str.split(" ");
-            charToMorse.put(str.charAt(0), splitStr[1]);
-            morseToChar.put(splitStr[1], str.charAt(0));
         }
-
-        in.close();
 
         if(charToMorse.isEmpty() || morseToChar.isEmpty()) {
             throw new IOException("Alphabet file is empty, nothing to do there");
