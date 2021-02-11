457f0aab3e3839a6b0aab51c7066cc1f592daeb7
dyuganov-n <dyuganov.n@gmail.com>
2021-02-11 16:06:09 +0700

    DRAW command changes

--- lab2_console_interpreter/src/ru/nsu/Dyuagnov/LogoWorld/Commands/DrawCommand.java
+++ lab2_console_interpreter/src/ru/nsu/Dyuagnov/LogoWorld/Commands/DrawCommand.java
@@ -2,6 +2,7 @@ package ru.nsu.Dyuagnov.LogoWorld.Commands;
 
 import ru.nsu.Dyuagnov.LogoWorld.Executor.Executor;
 import ru.nsu.Dyuagnov.LogoWorld.Field.Field;
+import ru.nsu.Dyuagnov.LogoWorld.Field.Object;
 
 
 public class DrawCommand implements Command {
@@ -16,6 +17,7 @@ public class DrawCommand implements Command {
 
     @Override
     public void execute() {
-
+        executor.draw();
+        field.setObject(executor.getCoordinates(), Object.FILLED);
     }
 }
