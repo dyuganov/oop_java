835c9505e72155e4a80be38887664e6b58c57cf8
dyuganov-n <dyuganov.n@gmail.com>
2021-02-21 22:10:49 +0700

    command factory decomposition

--- lab2_console_interpreter/src/main/java/ru/nsu/dyuganov/logoworld/CommandFactories/CommandFactory.java
+++ lab2_console_interpreter/src/main/java/ru/nsu/dyuganov/logoworld/CommandFactories/CommandFactory.java
@@ -33,34 +33,60 @@ public class CommandFactory {
      *                    <p>
      *                    Checks that first command is "INIT".
      */
-    public Command create(final String commandName) throws IOException, ReflectiveOperationException {
+    public Command create(final String commandName) throws IOException {
         logger.debug("Command creation started.");
         logger.debug("Got args command name: " + commandName);
+
         if (commands.containsKey(commandName)) {
             logger.info("Found command in cache.");
             logger.debug("Command creation finished.");
             return commands.get(commandName);
-        } else {
-            if (!commands.containsKey("INIT") && !commandName.equals("INIT")) {
-                logger.error("Command name is not INIT and no INIT command found in cache.");
-                System.err.println("First command must be INIT.");
-                return null;
-            }
-            final Properties properties = new Properties();
-            String commandsFileName = "src/main/resources/ru.nsu.dyuganov.logoworld/commandlist.properties";
-            try (BufferedReader propertiesReader = new BufferedReader(new FileReader(commandsFileName))) {
-                properties.load(propertiesReader);
-                logger.debug("Loaded file with command classes names.");
-            }
-            logger.debug("Properties file loaded.");
+        }
+        if (!commands.containsKey("INIT") && !commandName.equals("INIT")) {
+            logger.error("Command name is not INIT and no INIT command found in cache.");
+            System.err.println("First command must be INIT.");
+            return null;
+        }
+
+        String commandsFileName = "src/main/resources/ru.nsu.dyuganov.logoworld/commandlist.properties";
+        final Properties properties = getProperties(commandsFileName);
+        Command newCommand = createCommand(commandName, properties);
+        commands.put(commandName, newCommand);
+
+        logger.debug("Command put to cache.");
+        logger.debug("Command creation finished.");
+        return newCommand;
+    }
+
+    private Properties getProperties(final String commandsFileName) throws IOException {
+        final Properties properties = new Properties();
+        try (BufferedReader propertiesReader = new BufferedReader(new FileReader(commandsFileName))) {
+            properties.load(propertiesReader);
+            logger.debug("Loaded file with command classes names.");
+        }
+        return properties;
+    }
+
+    private Command createCommand(final String commandName, final Properties properties){
+        logger.debug("Properties file loaded.");
+        Command newCommand = null;
+        try {
             Class loadedClass = Class.forName(properties.getProperty(commandName));
             logger.debug("Loaded class from properties by name.");
-            Command newCommand = (Command) loadedClass.getConstructor().newInstance();
+            newCommand = (Command) loadedClass.getConstructor().newInstance();
             logger.info("Created new command.");
-            commands.put(commandName, newCommand);
-            logger.debug("Command put to cache.");
+        } catch (ReflectiveOperationException | RuntimeException e) {
+            logger.error("Can't find command in properties or other problem while creating command: " + e.getStackTrace());
+            System.err.println("Can't create command.");
             logger.debug("Command creation finished.");
-            return newCommand;
+            return null;
         }
+        return newCommand;
     }
 }
+
+
+
+
+
+
