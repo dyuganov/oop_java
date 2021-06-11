9579f0a84a340bbf04b79e8962ae19012ef903e0
dyuganov-n <dyuganov.n@gmail.com>
2021-06-12 04:52:52 +0700

    thread pool changes

--- lab3_tron/Server/src/main/java/ru/nsu/dyuganov/server/ThreadPool/ThreadPool.java
+++ lab3_tron/Server/src/main/java/ru/nsu/dyuganov/server/ThreadPool/ThreadPool.java
@@ -1,26 +1,85 @@
 package main.java.ru.nsu.dyuganov.server.ThreadPool;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ThreadPool {
-    Map<Integer, Thread> threads = new HashMap<>();
-    Queue<>
-    Thread mainThread;
+    Set<Thread> threadSet = new HashSet<>();
+    Queue<ThreadPoolTask> taskQueue = new ConcurrentLinkedQueue<>();
 
-    public ThreadPool(int size){
-        for(int i = 0; i < size; ++i){
-
+    public ThreadPool(int poolSize){
+        for(int i = 0; i < poolSize; ++i){
+            threadSet.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(!Thread.interrupted()){
+                        if(!taskQueue.isEmpty()) {
+                            taskQueue.poll().execute();
+                        } else{
+                            waitForTasks();
+                        }
+                    }
+                }
+            }));
         }
     }
 
+    /**
+     * Executes task. If there are non-started threads, start them.
+     * */
+    public void execute(ThreadPoolTask task){
+        if(task == null) return;
+        this.taskQueue.add(task);
+        notifyAll();
+
+        if(!taskQueue.isEmpty() && !isAllThreadsRunning()) {
+            int tasksCnt = taskQueue.size();
+            startThreads(tasksCnt);
+        }
+    }
+
+    /**
+     * Starts only created threads. If tasksQueue.size is less than threadPool.size, starts tasksQueue.size threads.
+     * */
+    private void startThreads(int threadsToStartNum){
+        for(Thread thread : threadSet){
+            if(!thread.isAlive() && threadsToStartNum > 0) thread.start();
+            --threadsToStartNum;
+        }
+    }
+
+    private boolean isAllThreadsRunning(){
+        boolean result = true;
+        for(Thread thread : threadSet){
+            if (!thread.getState().equals(Thread.State.RUNNABLE)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private void waitForTasks() {
+        while(taskQueue.isEmpty()){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * Starts only one thread of pool. Other threads will be started if needed.
+     * */
     public void start(){
-        mainThread.start();
+        threadSet.iterator().next().start();
     }
 
-    public void stop(){
-        mainThread.interrupt();
+    public void interruptAll(){
+        for(Thread thread : threadSet){
+            thread.interrupt();
+        }
     }
-
 }
