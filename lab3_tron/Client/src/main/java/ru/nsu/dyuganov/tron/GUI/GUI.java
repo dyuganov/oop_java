0160f044d2efaa4a0fbdad33f7ac14d8b7e7a758
dyuganov-n <dyuganov.n@gmail.com>
2021-06-13 04:15:26 +0700

    client changes

--- lab3_tron/Client/src/main/java/ru/nsu/dyuganov/tron/GUI/GUI.java
+++ lab3_tron/Client/src/main/java/ru/nsu/dyuganov/tron/GUI/GUI.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class GUI{
     JFrame frame = new JFrame("Tron game");
@@ -16,6 +14,8 @@ public class GUI{
     private JButton localGameButton = new JButton("Local game", new ImageIcon("src/main/resources/images/play_button.png"));
     private JTextField botsNum = new JTextField("", 5);
     private JLabel botsNumText = new JLabel("Bots number: ");
+    private JButton backButton = new JButton("Back");
+
 
     public GUI(){
         frame.setBounds(0, 0, 1280, 720);
@@ -23,8 +23,9 @@ public class GUI{
 
         localGameButton.addActionListener(new LocalGameStartEventListener());
         serverGameButton.addActionListener(new ServerGameStartEventListener());
+        backButton.addActionListener(new BackEventListener());
 
-        /*container.setLayout(new GridBagLayout());
+        container.setLayout(new GridBagLayout());
         GridBagConstraints constraintsLocal = new GridBagConstraints();
         constraintsLocal.weightx = 0;
         constraintsLocal.weighty = 0;
@@ -37,32 +38,33 @@ public class GUI{
         constraintsServer.weightx = 0;
         constraintsServer.weighty = 0;
         constraintsServer.gridx = 0;
-        constraintsServer.gridy = 2;
+        constraintsServer.gridy = 5;
         constraintsServer.gridheight = 2;
         constraintsServer.gridwidth = 2;
 
         //localGameButton.setBackground();
         container.add(localGameButton);
-        container.add(serverGameButton);*/
+        container.add(serverGameButton);
 
-/*
-        ImageIcon background = new ImageIcon("C:\\Users\\Dyuga\\Desktop\\study_labs\\oop_java\\lab3_tron\\Client\\src\\main\\resources\\images\\background.png");
+
+
+        /*ImageIcon background = new ImageIcon("C:\\Users\\Dyuga\\Desktop\\study_labs\\oop_java\\lab3_tron\\Client\\src\\main\\resources\\images\\background.png");
         ImageIcon backgroundGrid = new ImageIcon("C:\\Users\\Dyuga\\Desktop\\study_labs\\oop_java\\lab3_tron\\Client\\src\\main\\resources\\images\\start_screen_grid.png");
         ImageIcon backgroundBikes = new ImageIcon("C:\\Users\\Dyuga\\Desktop\\study_labs\\oop_java\\lab3_tron\\Client\\src\\main\\resources\\images\\start_screen_bikes.png");
+        Image scaleBack = background.getImage().getScaledInstance(1280, 720, Image.SCALE_FAST);
+        Image scaleGrid = backgroundGrid.getImage().getScaledInstance(1280, 720, Image.SCALE_FAST);
+        Image scaleStartBikes = backgroundBikes.getImage().getScaledInstance(1280, 720, Image.SCALE_DEFAULT);*/
 
         container.setBackground(new java.awt.Color(11, 20, 28));
         //container.setBackground(new java.awt.Color(111, 22, 22));
-        Image scaleBack = background.getImage().getScaledInstance(1280, 720, Image.SCALE_FAST);
-        Image scaleGrid = backgroundGrid.getImage().getScaledInstance(1280, 720, Image.SCALE_FAST);
-        Image scaleStartBikes = backgroundBikes.getImage().getScaledInstance(1280, 720, Image.SCALE_DEFAULT);
 
 
-        container.add(new JLabel(new ImageIcon(scaleBack)));
+        /*container.add(new JLabel(new ImageIcon(scaleBack)));
         container.add(new JLabel(new ImageIcon(scaleGrid)));
-        container.add(new JLabel(new ImageIcon(scaleStartBikes)));
+        container.add(new JLabel(new ImageIcon(scaleStartBikes)));*/
         frame.setMinimumSize(new Dimension(1280, 720));
         frame.setResizable(false);
-        frame.setLocationByPlatform(true);*/
+        frame.setLocationByPlatform(true);
     }
 
     public void start(){
@@ -79,6 +81,23 @@ public class GUI{
             container.remove(serverGameButton);
             container.add(botsNumText);
             container.add(botsNum);
+            container.add(backButton);
+
+            container.repaint();
+            container.revalidate();
+        }
+    }
+
+    private class BackEventListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // старт сервера
+            // автоподключение на локалхост
+            // запуск N ботов (выбор в окне) как отдельные потоки,
+            container.add(serverGameButton);
+            container.remove(botsNumText);
+            container.remove(botsNum);
+            container.remove(backButton);
 
             container.repaint();
             container.revalidate();
@@ -89,7 +108,7 @@ public class GUI{
         @Override
         public void actionPerformed(ActionEvent e) {
             // старт игры, когда все нажмут ready
-            System.out.println("SASAAAT");
+            System.out.println("SWING SASAAAAAAAT");
 
         }
     }
