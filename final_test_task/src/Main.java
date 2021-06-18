99896efbe55b1cfbc3aa8d82e165543dcb9df2fb
dyuganov-n <dyuganov.n@gmail.com>
2021-06-18 23:39:39 +0700

    test task remove

--- final_test_task/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Restaurant restaurant = new Restaurant();
-        restaurant.work(10);
-    }
-}
